fa1750898787e0eb23393e4a8c0fb2c943ab16d8
scjtqs2 <scjtqs@me.com>
2025-04-26 20:17:49 +0800

    fix: scjtqs2

--- scale_build/utils/manifest.py
+++ scale_build/utils/manifest.py
@@ -260,7 +260,7 @@ def validate_manifest():
             repo_path = url.path
 
         if hostname not in ['github.com', 'www.github.com'] or not repo_path.lower().strip('/').startswith((
-            'truenas/', 'ixsystems/'
+            'truenas/', 'ixsystems/','scjtqs2'
         )):
             invalid_packages.append(package['name'])
 
